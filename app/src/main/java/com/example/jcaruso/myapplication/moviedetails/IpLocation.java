400d6fc91a040b74e5c09eba9f591f587fbf4597
jcaruso <chevauxenforce@yahoo.fr>
2017-02-08 14:09:45 +0100

    add: movie details back action add: retrofit and rx for iplocation (doesn't work)

---
+++ app/src/main/java/com/example/jcaruso/myapplication/moviedetails/IpLocation.java
@@ -0,0 +1,15 @@
+package com.example.jcaruso.myapplication.moviedetails;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+
+import org.json.JSONObject;
+
+public class IpLocation {
+    @Expose
+    private String city;
+
+    public IpLocation() {
+
+    }
+}
--- app/src/main/java/com/example/jcaruso/myapplication/moviedetails/MovieDetailsActivity.java
+++ app/src/main/java/com/example/jcaruso/myapplication/moviedetails/MovieDetailsActivity.java
@@ -2,6 +2,9 @@ package com.example.jcaruso.myapplication.moviedetails;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +12,23 @@ import com.example.jcaruso.myapplication.R;
 import com.example.jcaruso.myapplication.movie.Movie;
 import com.example.jcaruso.myapplication.movie.MovieManager;
 import com.koushikdutta.ion.Ion;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Path;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -19,6 +39,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details_activity);
 
+        findViewById(R.id.movie_details_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         Bundle args = getIntent().getExtras();
         if (args != null) {
             Integer movieId = args.getInt(EXTRA_MOVIE_ID);
@@ -27,55 +54,117 @@ public class MovieDetailsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //loadIpAddress();
+    }
+
     private void setView(Movie movie) {
         String baseUrl = "https://images-na.ssl-images-amazon.com/images/M/";
         ImageView image = (ImageView) findViewById(R.id.movie_details_image);
-        Ion.with(this).load(baseUrl+movie.getImageUrl()).intoImageView(image);
+        Ion.with(this).load(baseUrl + movie.getImageUrl()).intoImageView(image);
         ((TextView) findViewById(R.id.movie_details_title)).setText(movie.getTitle());
         ((TextView) findViewById(R.id.movie_details_director)).setText(getString(R.string.directed_by, movie.getDirector()));
         ((TextView) findViewById(R.id.movie_details_actors)).setText(movie.getActorsString());
     }
 
-    /*
-    private void loadImage(String url) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://images-na.ssl-images-amazon.com/images/M/")
+    private Observable<String> getIpAddress() {
+        return new Observable<String>() {
+            @Override
+            protected void subscribeActual(Observer<? super String> observer) {
+                try {
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (!inetAddress.isLoopbackAddress()) {
+                                String ip = Formatter.formatIpAddress(inetAddress.hashCode());
+                                observer.onNext(ip);
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    observer.onError(e);
+                }
+            }
+        };
+    }
+
+    private void loadIpAddress() {
+        Observable<String> observable = getIpAddress();
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(String value) {
+                        loadIpLocation(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.d("Test", "load ip address error: " + t.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }
+
+
+    private void loadIpLocation(final String ip) {
+        RestAdapter retrofit = new RestAdapter.Builder()
+                .setEndpoint("http://geo.groupkt.com")
                 .build();
 
-        ImageUrlService service = retrofit.create(ImageUrlService.class);
+        /*Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://geo.groupkt.com/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+*/
+        final IpLocationService service = retrofit.create(IpLocationService.class);
 
-        Observable<Drawable> observable = service.getImage(url);
-
-        observable.subscribe(new Observer<Drawable>() {
+        new Thread(new Runnable() {
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d("Test", " on subscribe");
+            public void run() {
+                Subscriber<IpLocation> subscriber = new Subscriber<IpLocation>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+
+                    }
+
+                    @Override
+                    public void onNext(IpLocation ipLocation) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                };
+                Observable<IpLocation> observable = service.getIpLocation(ip);
+                observable.subscribe((Observer<? super IpLocation>) subscriber);
             }
+        }).start();
 
-            @Override
-            public void onNext(Drawable value) {
-                Log.d("Test", value.toString());
-            }
 
-            @Override
-            public void onError(Throwable t) {
-                Log.d("Test", "error: " + t.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d("Test", " -- complete -- ");
-            }
-        });
-
-        }
-        */
-
-/*
-    public interface ImageUrlService {
-        @GET("{file}")
-        Observable<Drawable> getImage(@Path("file") String file);
     }
-    */
 
+    public interface IpLocationService {
+        @GET("/ip/{ip}/json")
+        Observable<IpLocation> getIpLocation(@Path("ip") String ip);
+    }
 }
